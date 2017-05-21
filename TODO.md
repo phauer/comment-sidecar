094fc031ba9edaadc389a9602ce98faf045df6fe
Philipp Hauer <info@philipphauer.de>
2017-05-21 20:12:24 +0200

    convert db results to json. order desc. use epoch timestamp instead of formatted string for better support for different timezones.

---
+++ TODO.md
@@ -0,0 +1,5 @@
+
+- db:
+    - index correctly used? use `explain`.
+    - insert with different timezone? -> insert as unix timestamp not as string
+    - use epoch timestamp or datetime with timezone for read and write 
--- src/comment-sidecar.php
+++ src/comment-sidecar.php
@@ -1,55 +1,37 @@
 <?php
+function process(){
+    $method = $_SERVER['REQUEST_METHOD'];
+    $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+    $input = json_decode(file_get_contents('php://input'),true);
 
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-$input = json_decode(file_get_contents('php://input'),true);
+    //docker-compose creates links and adds entries to /etc/hosts -> use "commentsidecar_mysql_1" instead of "localhost"
+    $dbhost = 'commentsidecar_mysql_1';
+    $dbname = 'comment-sidecar';
+    $dbuser = 'root';
+    $dbpw = 'root';
+    $dbport = 3306;
+    try {
+        $handler = new PDO("mysql:host=$dbhost:$dbport;dbname=$dbname", $dbuser, $dbpw);
+        $handler->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+        $stmt = $handler->prepare("SELECT id, author, email, content, reply_to as replyTo, site, path, unix_timestamp(creation_date) as creationTimestamp FROM comments order by creation_date desc;");
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        header('Content-Type: application/json');
+        $json=json_encode($results);
+        echo $json;
+    } catch(PDOException $exception) {
+        echo $exception->getMessage();
+    }
+    $handler = null; //close connection
 
-//TODO PDO supports object mapping!
-
-//docker-compose creates links and adds entries to /etc/hosts -> use "commentsidecar_mysql_1" instead of "localhost"
-$dbhost = 'commentsidecar_mysql_1';
-$dbname = 'comment-sidecar';
-$dbuser = 'root';
-$dbpw = 'root';
-$dbport = 3306;
-try {
-    $handler = new PDO("mysql:host=$dbhost:$dbport;dbname=$dbname", $dbuser, $dbpw);
-    $handler->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-//    $stmt = $handler->prepare("SELECT version();");
-    $stmt = $handler->prepare("SELECT * from comments;");
-//    $stmt = $handler->prepare("Normales MySQL, z.B. SELECT * FROM table WHERE name = :name");
-//    $stmt->bindParam(':name', $name);
-    $stmt->execute();
-    $allResults = $stmt->fetchAll();
-    echo $allResults[0]["version()"];
-} catch(PDOException $e) {
-    echo $e->getMessage();
+    //switch ($method) {
+    //    case 'GET': break;
+    //}
+    //
+    //if (!$result) {
+    //    http_response_code(404);
+    //    die(mysqli_error($link));
+    //}
 }
-$handler = null; //close connection
 
-//$link = mysqli_connect('commentsidecar_mysql_1', 'root','root',  'comment-sidecar', 3306);
-//mysqli_set_charset($link,'utf8');
-//mysqli_real_escape_string
-
-//switch ($method) {
-//    case 'GET': $sql = "select version();"; break;
-////    case 'GET': $sql = "select * from `$table`".($key?" WHERE id=$key":''); break;
-//}
-//
-//$result = mysqli_query($link,$sql);
-//
-//if (!$result) {
-//    http_response_code(404);
-//    die(mysqli_error($link));
-//}
-//
-//if ($method == 'GET') {
-//    header('Content-Type: application/json');
-//    echo '[';
-//    for ($i=0;$i<mysqli_num_rows($result);$i++) {
-//        echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
-//    }
-//    echo ']';
-//}
-//
-//mysqli_close($link);
+process();
