e4ce49cfabbacbeae358a74358ad9f1b6eb0c53f
Philipp Hauer <info@philipphauer.de>
2017-07-16 21:40:49 +0200

    increase size of database fields. just to be sure.

--- sql/create-comments-table.sql
+++ sql/create-comments-table.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `author` varchar(30) NOT NULL,
-  `email` varchar(30) DEFAULT NULL,
+  `author` varchar(40) NOT NULL,
+  `email` varchar(40) DEFAULT NULL,
   `content` text NOT NULL,
   `reply_to` int(11) DEFAULT NULL,
-  `site` varchar(35) NOT NULL,
-  `path` varchar(150) NOT NULL,
+  `site` varchar(40) NOT NULL,
+  `path` varchar(170) NOT NULL,
   `creation_date` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
