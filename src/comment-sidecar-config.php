3c8b83300cf63a81b5a11e3c263f886b139dbeea
Philipp Hauer <info@philipphauer.de>
2017-07-21 19:38:48 +0200

    suddenly, somehow host network mode is not required for xdebug anymore... (different docker versions?)

--- src/comment-sidecar-config.php
+++ src/comment-sidecar-config.php
@@ -3,7 +3,6 @@ const LANGUAGE = "en"; # see translations folder for supported languages
 const SITE = "localhost"; # key for this site to identity comment of this site
 const E_MAIL_FOR_NOTIFICATIONS = "test@localhost.de";
 const DB_HOST = 'commentsidecar_mysql_1'; # to access from host system, use 127.0.0.1
-//const DB_HOST = 'localhost'; # network_mode=host
 const DB_NAME = 'comment-sidecar';
 const DB_USER = 'root';
 const DB_PW = 'root';
