af7c0894bd6f521becee17c0e15a5e6628e609ba
Bart Nijssen <bart@bartnijssen.com>
2020-06-09 14:11:56 +0200

    Added Dutch (nl) translation

---
+++ src/translations/nl.php
@@ -0,0 +1,31 @@
+<?php
+$translations = array(
+    'name' => 'Naam',
+    'email' => 'E-Mail',
+    'comment' => 'Reactie',
+
+    'emailHint' => 'Een e-mailadres is niet verplicht. Je e-mailadres wordt gebruikt om je een notificatie te sturen wanneer iemand heeft gereageerd. Je e-mailadres zal nooit worden gepubliceerd. Je kunt je afmelden voor notificaties in elke e-mail die je ontvangt.',
+
+    'submit' => 'Voeg toe',
+    'reply' => 'Antwoord',
+    'writeComment' => 'Schrijf een reactie...',
+
+    'noCommentsYet' => 'Er zijn nog geen reacties',
+    'successMessage' => 'Je reactie is toegevoegd.',
+    'failMessage' => "Kon je reactie niet toevoegen. Fout: ",
+
+    'dateString' => '{} geleden',
+    'years' => 'jaren',
+    'months' => 'maanden',
+    'days' => 'dagen',
+    'hours' => 'uren',
+    'minutes' => 'minuten',
+    'seconds' => 'seconden',
+
+    # reply notification e-mail
+    'subject' => 'Antwoord op je reactie van {}',
+    'introduction' => 'er is een antwoord op je reactie.',
+    'author' => 'Auteur',
+    'message' => 'Bericht',
+    'unsubscribeDescription' => 'Om je uit te schrijven voor notificaties als deze, klik op de volgende link:',
+);
